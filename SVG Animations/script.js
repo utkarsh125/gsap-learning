8d52da0c4b2232f6e5b124194f2e514d701b91ab
Utkarsh Pandey <utk2665@gmail.com>
2024-06-04 12:15:29 +0530

    SVG Animation needs revision

---
+++ SVG Animations/script.js
@@ -0,0 +1,11 @@
+var initialPath = `M 10 80 Q 95 10 180 80`;
+var final = `M 10 80 Q 95 10 180 80`;
+
+
+var string = document.querySelector("#string")
+
+
+string.addEventListener("mouseenter", function(){
+    console.log('clicked');
+
+})
